c7c39f5cdd38eea47f85b85c372713feb8856225
toru <toru.imamura0514@gmai.com>
2022-11-14 21:57:09 +0900

    homepage photoUrl edit

--- src/App.js
+++ src/App.js
@@ -10,9 +10,19 @@ import satine from './images/satine.jpeg'
 import purpleheart from './images/purpleheart.jpeg'
 import cloud9 from './images/cloud9.jpeg'
 
-function App () {
-  const openUrlTab = () =>{
-    window.open("https://www.creema.jp/creator/7614629/item/onsale")
+const padaukUrl = "https://www.creema.jp/item/14942878/detail"
+const rosewoodUrl = "https://www.creema.jp/item/14942931/detail"
+const wengeUrl = "https://www.creema.jp/item/14943033/detail"
+const mapleUrl = "https://www.creema.jp/item/14943075/detail"
+const snakewoodUrl = "https://www.creema.jp/item/14943228/detail"
+const pinkivoryUrl = "https://www.creema.jp/item/14943315/detail"
+const karinUrl = "https://www.creema.jp/item/14943464/detail"
+const satineUrl = "https://www.creema.jp/item/14943485/detail"
+const purpleheartUrl = "https://www.creema.jp/item/14943547/detail"
+
+  function App () {
+  const openUrlTab = (url) =>{
+    window.open(url)
   }
   return (
     <div className="App">
@@ -26,28 +36,44 @@ function App () {
   </button>
       <br />
       <img className='purpleheart-image'
-           src={purpleheart} alt="パープルハート写真"/>
+           src={purpleheart}
+           alt="パープルハート写真"
+           onClick={()=>openUrlTab(purpleheartUrl)}/>
       <img className='satine-image'
-           src={satine} alt="サティーネ写真"/>
+           src={satine}
+           alt="サティーネ写真"
+           onClick={()=>openUrlTab(satineUrl)}/>
       <img className='karin-image'
-           src={karin} alt="花梨写真"/>
+           src={karin}
+           alt="花梨写真"
+           onClick={()=>openUrlTab(karinUrl)}/>
       <br />
       <img className='pinkivory-image'
-           src={pinkivory} alt="ピンクアイボリー写真"/>
+           src={pinkivory}
+           alt="ピンクアイボリー写真"
+           onClick={()=>openUrlTab(pinkivoryUrl)}/>
       <img className='snakewood-image'
-           src={snakewood} alt="スネークウッド写真"/>
+           src={snakewood}
+           alt="スネークウッド写真"
+           onClick={()=>openUrlTab(snakewoodUrl)}/>
       <img className='maple-image'
-           src={maple} alt="メープル写真"/>
+           src={maple}
+           alt="メープル写真"
+           onClick={()=>openUrlTab(mapleUrl)}/>
       <br />
       <img className='wenge-image'
-           src={wenge} alt="ウェンジ写真"/>
+           src={wenge}
+           alt="ウェンジ写真"
+           onClick={()=>openUrlTab(wengeUrl)}/>
       <img className='rosewood-image'
-           src={rosewood} alt="ローズウッド写真"/>
+           src={rosewood}
+           alt="ローズウッド写真"
+           onClick={()=>openUrlTab(rosewoodUrl)}/>
       <img className='padauk-image'
-           src={padauk} alt="パドック写真"/>
+           src={padauk}
+           alt="パドック写真"
+           onClick={()=>openUrlTab(padaukUrl)}/>
       <br />
-
-
     </div>
   )
 }
