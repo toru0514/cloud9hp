bd6f690d9ca2f4d7dde80abf8f2b500ef8535943
河本 勇人 <kawamoto.web@gmail.com>
2022-12-09 01:53:08 +0900

    Footer をコンポーネントに切り出し

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import './assets/styles/reset.css'
 import './App.css'
 import { RouteView } from './components/RouteView'
-import { FooterMenu } from './components/FooterMenu'
-import { SNSLinks } from './components/SNSLinks'
-import { Copyright } from './components/Copyright'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App () {
   return (
     <div>
       <Header/>
       <RouteView/>
-      <FooterMenu>
-        <FooterMenu.Item to="/">Home</FooterMenu.Item>
-        <FooterMenu.Item to="/product">Product</FooterMenu.Item>
-        <FooterMenu.Item to="/support">Support</FooterMenu.Item>
-        <FooterMenu.Item to="/contact">Contact</FooterMenu.Item>
-      </FooterMenu>
-      <SNSLinks/>
-      <Copyright/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { FooterMenu } from './FooterMenu'
+import { SNSLinks } from './SNSLinks'
+import { Copyright } from './Copyright'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <FooterMenu>
+        <FooterMenu.Item to="/">Home</FooterMenu.Item>
+        <FooterMenu.Item to="/product">Product</FooterMenu.Item>
+        <FooterMenu.Item to="/support">Support</FooterMenu.Item>
+        <FooterMenu.Item to="/contact">Contact</FooterMenu.Item>
+      </FooterMenu>
+      <SNSLinks/>
+      <Copyright/>
+    </footer>
+  )
+}
