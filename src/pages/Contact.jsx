6fb46d7a73c0b3ba0d363f19abd025af0dfc07fd
toru <toru.imamura0514@gmai.com>
2022-12-18 05:11:21 +0900

    contact page add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import './Contact.css'
 
 class ContactPage extends React.Component {
   render () {
     return (
-      <div className="contact-page">
-        Contact Pages
+      <div className="container">
+        <h1 className="contact-page">お問い合わせ</h1>
+        <form name="contact" method="POST" data-netlify="true">
+          <div>
+            <label htmlFor="name">お名前（必須）</label>
+            <input name="name" type="text" required/>
+          </div>
+          <div>
+            <label htmlFor="email">メールアドレス（必須）</label>
+            <input name="email" type="email" required/>
+          </div>
+          <div>
+            <label htmlFor="content">お問い合わせ内容</label>
+            <textarea
+              name="content"
+              rows="10"
+              required
+            />
+          </div>
+          <button className="send-button" type="submit">送信する</button>
+        </form>
       </div>
     )
   }
