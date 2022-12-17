3af139bc1c2b5f0c6592da0d25384e5a2386c315
toru <toru.imamura0514@gmai.com>
2022-12-17 22:08:45 +0900

    product page edit for all

--- src/data/index.js
+++ src/data/index.js
@@ -15,7 +15,7 @@ import padaukImage from '../assets/images/padauk.png'
 
 export const shop = {
   url: 'https://www.creema.jp/creator/7614629/item/onsale',
-  image: cloud9Image
+  image: cloud9Image,
 }
 
 export const links = [
@@ -23,26 +23,26 @@ export const links = [
     id: 'creema',
     url: 'https://www.creema.jp/creator/7614629/item/onsale',
     image: creemaImage,
-    name: 'creema'
+    name: 'creema',
   },
   {
     id: 'minne',
     url: 'https://minne.com/@cloud9-wood',
     image: minneImage,
-    name: 'minne'
+    name: 'minne',
   },
   {
     id: 'instagram',
     url: 'https://instagram.com/cloud9_woodwork?igshid=YmMyMTA2M2Y=',
     image: instagramImage,
-    name: 'Instagram'
+    name: 'Instagram',
   },
   {
     id: 'twitter',
     url: 'https://twitter.com/cloud9_woodwork?s=21&t=bdwddBl2tEFpUtr9tMjI5w',
     image: twitterImage,
-    name: 'Twitter'
-  }
+    name: 'Twitter',
+  },
 ]
 
 export const products = [
@@ -50,54 +50,81 @@ export const products = [
     id: 'purpleheart',
     url: 'https://www.creema.jp/item/14943547/detail',
     image: purpleheartImage,
-    name: 'パープルハート'
+    enName: 'cloud9 WoodRing PURPLE HEART',
+    jpName: 'cloud9 ウッドリング パープルハート',
+    intro: '「信頼」や「確実性」の意味をともなうパープルカラーの指輪。指に取り入れるだけでいつもより大人な雰囲気に包まれます。',
+    name: 'パープルハート',
   },
   {
     id: 'satine',
     url: 'https://www.creema.jp/item/14943485/detail',
     image: satineImage,
-    name: 'サティーネ'
+    enName: 'cloud9 WoodRing SATINE',
+    jpName: 'cloud9 ウッドリング サティーネ',
+    intro: '「深紅の赤」と言われる見れば見るほど美しい指輪。金色の筋が現れ、見る方向によって色合いと光沢の変化が感じられます。',
+    name: 'サティーネ',
   },
   {
     id: 'karin',
     url: 'https://www.creema.jp/item/14943464/detail',
     image: karinImage,
-    name: '花梨'
+    enName: 'cloud9 WoodRing KARIN',
+    jpName: 'cloud9 ウッドリング カリン',
+    intro: 'とても木肌が美しく、綺麗な光沢を持った指輪。男性にも女性にも、年齢問わず幅広く合わせることができます。ペアリングにもオススメです。',
+    name: '花梨',
   },
   {
     id: 'pinkivory',
     url: 'https://www.creema.jp/item/14943315/detail',
     image: pinkivoryImage,
-    name: 'ピンクアイボリー'
+    enName: 'cloud9 WoodRing PINK IVORY',
+    jpName: 'cloud9 ウッドリング ピンクアイボリー',
+    intro: '宝石のように非常に高価で希少なピンクアイボリー。天然色とは思えないほど綺麗なピンク色をしており、”木材の女王”と言われています。',
+    name: 'ピンクアイボリー',
   },
   {
     id: 'snakewood',
     url: 'https://www.creema.jp/item/14943228/detail',
+    enName: 'cloud9 WoodRing SNAKE WOOD',
+    jpName: 'cloud9 ウッドリング スネークウッド',
+    intro: '木材の”王様”スネークウッド。男性的で力強い指輪は、着けることで圧倒的な格好良さと色気で他人と差をつけることができます。',
     image: snakewoodImage,
-    name: 'スネークウッド'
+    name: 'スネークウッド',
   },
   {
     id: 'maple',
     url: 'https://www.creema.jp/item/14943075/detail',
     image: mapleImage,
-    name: 'メープル'
+    enName: 'cloud9 WoodRing MAPLE',
+    jpName: 'cloud9 ウッドリング メープル',
+    intro: '白く真珠のような輝きを放つメープル。白いメープルの指輪を着けることで、純白で汚れのない…そんな印象を与えることができます。',
+    name: 'メープル',
   },
   {
     id: 'wenge',
     url: 'https://www.creema.jp/item/14943033/detail',
     image: wengeImage,
+    enName: 'cloud9 WoodRing WENGE',
+    jpName: 'cloud9 ウッドリング ウェンジ',
+    intro: '黒く落ち着いた色調と独特で繊細な木目が美しいウェンジ。そんな指輪を着けるとシックで大人な印象に仕立ててくれます。',
     name: 'ウェンジ',
   },
   {
     id: 'rosewood',
     url: 'https://www.creema.jp/item/14942931/detail',
     image: rosewoodImage,
-    name: 'ローズウッド'
+    enName: 'cloud9 WoodRing ROSE WOOD',
+    jpName: 'cloud9 ウッドリング ローズウッド',
+    intro: '古くから高級家具等で使用されていたローズウッド。黒紫色で美しい最高級の指輪を着けて日々をいつも以上に楽しくお過ごしください。',
+    name: 'ローズウッド',
   },
   {
     id: 'padauk',
     url: 'https://www.creema.jp/item/14942878/detail',
     image: padaukImage,
-    name: 'パドック'
-  }
+    enName: 'cloud9 WoodRing PADAUK',
+    jpName: 'cloud9 ウッドリング パドック',
+    intro: '赤く美しいパドック。繊細だけどクールな赤、明るく前向きで情熱的な赤。そんなあなたの気持ちと共に寄り添ってくれる指輪です。',
+    name: 'パドック',
+  },
 ]
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Product.css'
-
-import purpleheartImage from '../assets/images/purpleheart.png'
+import { products } from '../data'
 
 class ProductPage extends React.Component {
   render () {
@@ -12,27 +11,33 @@ class ProductPage extends React.Component {
           <p className="title-jp">製品紹介</p>
           <br/>
           <p className="product-ring">Wood Ring</p>
-          <p>貴重な木材を使用するオリジナルのウッドリング。</p>
+          <p>希少な木材を使用するオリジナルのウッドリング。</p>
           <br/>
         </div>
         <br/>
-        <div className="purpleheart-area">
-          <div className="image-area">
-            <img className="image"
-                 src={purpleheartImage}
-                 alt="パープルハート写真"/>
-          </div>
-          <div className="description-area">
-            <h2>
-              <span className="product-title">cloud9 WoodRing PURPLEHEART</span>
-              <br/>
-              <span className="product-title">cloud9 ウッドリング パープルハート</span>
-            </h2>
-            <br/>
-            <span>「信頼」や「確実性」の意味をともなうパープルカラーの指輪。手にパープルを取り入れるだけでいつもより大人な雰囲気に包まれます。</span>
-            <br/><br/>
-            <p className="read-test">READ MORE</p>
-          </div>
+        <div className="ring-repertory-area">
+          {
+            products.map((product) => (
+              <>
+                <div className="image-area">
+                  <img className="image"
+                       src={product.image}
+                       alt={product.name}/>
+                </div>
+                <div className="description-area">
+                  <h2>
+                    <span className="product-title">{product.enName}</span>
+                    <br/>
+                    <span className="product-title">{product.jpName}</span>
+                  </h2>
+                  <br/>
+                  <span>{product.intro}</span>
+                  <br/><br/>
+                  <p className="read-text">READ MORE</p>
+                </div>
+              </>
+            ))
+          }
         </div>
       </div>
     )
