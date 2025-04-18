daa9e34ce8eb800ea07e6dea1eee510c5009e252
toru <toru.imamura0514@gmai.com>
2025-04-19 00:15:16 +0900

    プロダクト画面の背景色変更

--- src/components/Product/Bangle/Bangle.tsx
+++ src/components/Product/Bangle/Bangle.tsx
@@ -5,7 +5,7 @@ const BangleArea = () => {
     return (
         <div className="p-2 lg:p-4">
             <div className="lg:p-2 lg:px-20">
-                <div className="p-4 lg:p-6 bg-gray-200 ">
+                <div className="p-4 lg:p-6 bg-gray-100 ">
                     <h2 className="sm:text-xl md:text-3xl lg:text-3xl pl-2 md:font-semibold lg:font-semibold mb-2 md:mb-4 lg:mb-4 text-left">
                         ◆ WOOD BANGLE
                     </h2>
