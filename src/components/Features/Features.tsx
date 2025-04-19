84e985599b64b1778a6a41db8857e59d65a223db
toru <toru.imamura0514@gmai.com>
2025-04-19 18:39:20 +0900

    Featuresのタイトル修正

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -13,7 +13,7 @@ import Button from "@/components/ui/Button";
 
 const FeaturesArea = () => (
   <div className="p-6 lg:p-8 xl:p-10 bg-neutral-200 relative">
-    <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-center">FEATURES</h2>
+    <h2 className="text-2xl md:text-4xl lg:text-5xl font-semibold">FEATURES</h2>
 
     <div className="px-6 sm:pt-10 sm:pb-10 relative">
       <Swiper
